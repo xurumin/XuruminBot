49266ec2840796de03cf91c5c4810c62f55bff3c
jonatas araujo <jonatasaraujofx@gmail.com>
2020-10-29 11:22:27 -0300

    add cat bobbing cmd

---
+++ src/commands/catbobbing/catbobbing.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const database = require("./../../utils/database");
+const Utils = require("./../../utils/utils");
+
+const axios = require("axios").default;
+
+module.exports = {
+	validate(client, message) {
+		if (!message.member.hasPermission('MANAGE_GUILD')) {
+			throw new Error('no_permission');
+		}
+	},
+	/**
+	 * @param  {Discord.Client} client
+	 * @param  {Discord.Message} message
+	 * @param  {} args
+	 */
+	run: async (client, message, args) => {
+		return message.channel.send(
+			new Discord.MessageEmbed()
+			.setColor('#9d65c9')
+			.setTitle("Gatinho")
+			.setDescription("🎶🎶 🥳🥳🥳")
+			.setAuthor(client.user.username)
+			.setImage("https://i.imgur.com/OmiPKRQ.gif")
+		)
+	},
+	get command() {
+		return {
+			name: 'catbobbing',
+			aliases: ["gatodancando","gatinhodancando","gatobalancando","catbobbinghead"]
+		};
+	},
+};
