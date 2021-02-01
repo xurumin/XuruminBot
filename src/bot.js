bf782c09fe9544839d127ec5fd42239cfa02527c
jonatas araujo <jonatasaraujofx@gmail.com>
2021-02-01 18:21:01 -0300

    add: topgg status

--- src/bot.js
+++ src/bot.js
@@ -108,6 +108,25 @@ const init = async () => {
 
 	client.login(process.env.DISCORD_API)
 	client.on("ready", () => {
+		const DBL = require("dblapi.js");
+		const dbl = new DBL(process.env.TOPGG_API, client);
+
+		dbl.on('posted', () => {
+			console.log('Server count posted!');
+		})
+		
+		dbl.on('error', e => {
+		console.log(`Oops! ${e}`);
+		})
+
+		function postDBL(){
+			dbl.postStats(client.guilds.size, client.shard.ids[0]);
+		}
+		postDBL()
+		setInterval(async () => {
+			postDBL()
+		}, 1800000);
+
 		client.user.setActivity({
 			name: `Precisa de ajuda? ${process.env.COMMAND_PREFIX}help`
 		})
