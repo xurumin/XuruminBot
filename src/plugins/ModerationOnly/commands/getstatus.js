097d163141549ff34a25bb1558cb99225ab0ba32
jonatas araujo <jonatasaraujofx@gmail.com>
2021-03-04 13:51:58 -0300

    add: ModerationOnly cmds

---
+++ src/plugins/ModerationOnly/commands/getstatus.js
@@ -0,0 +1,47 @@
+"use strict";
+const Discord = require('discord.js');
+const Utils = require("./../../../utils/utils")
+const config = require("./../../../config");
+require('dotenv/config');
+
+
+const getVoiceConnectionsCount = async (client) => {
+    const req = await client.shard.fetchClientValues('voice.connections.size');
+    return req.reduce((p, n) => p + n, 0);
+  }
+  
+  const getServerCount = async (client) => {
+    const req = await client.shard.fetchClientValues('guilds.cache.size');
+    return req.reduce((p, n) => p + n, 0);
+  }
+
+const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`
+module.exports = {
+    validate(client, message) {
+        return true;
+    },
+    /**
+     * @param  {Discord.Client} client
+     * @param  {Discord.Message} message
+     * @param  {} args
+     */
+    run: async (client, message, args, LOCALE) => {
+        return new Promise(async (resolve, reject)=>{
+            if(!config.specialusers.includes(message.author.id)){
+                return message.channel.send("Sorry you can not send this command.")
+            }
+            var txt = "";
+            txt += `Voice connections: ${await getVoiceConnectionsCount(client)}\n`
+            txt+= `Server count: ${await getServerCount(client)}\n`
+            txt+= `Memory: ${formatMemoryUsage(process.memoryUsage().heapUsed)}/${formatMemoryUsage(process.memoryUsage().heapTotal)}`
+            return message.channel.send(txt)
+
+        })
+    },
+    get command() {
+        return {
+            name: "getstatus",
+            aliases: []
+        }
+    },
+};
