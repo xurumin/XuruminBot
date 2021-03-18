3532c5656e35d0c56b9b49c352c619811494123d
jonatas araujo <jonatasaraujofx@gmail.com>
2021-03-18 18:56:28 -0300

    update: animescream cmd

--- src/plugins/VideoGen/commands/animescream.js
+++ src/plugins/VideoGen/commands/animescream.js
@@ -16,21 +16,30 @@ module.exports = {
      */
     run: (client, message, args, LOCALE) => {
         return new Promise(async (resolve, reject) => {
-            const isPremium = await Utils.Profile.isPremium(client, message.author.id)
-            if (!isPremium) {
-                const notPremiumEmbed = new Discord.MessageEmbed()
-                    .setColor('#9d65c9')
-                    .setTitle(LOCALE.errors.user_is_not_premium.title)
-                    .setDescription(LOCALE.errors.user_is_not_premium.description.interpolate({
-                        prefix: process.env.COMMAND_PREFIX
-                    }))
-                return resolve(message.channel.send(notPremiumEmbed))
+            // const isPremium = await Utils.Profile.isPremium(client, message.author.id)
+            // if (!isPremium) {
+            //     const notPremiumEmbed = new Discord.MessageEmbed()
+            //         .setColor('#9d65c9')
+            //         .setTitle(LOCALE.errors.user_is_not_premium.title)
+            //         .setDescription(LOCALE.errors.user_is_not_premium.description.interpolate({
+            //             prefix: process.env.COMMAND_PREFIX
+            //         }))
+            //     return resolve(message.channel.send(notPremiumEmbed))
+            // }
+            if (!message.channel.nsfw) {
+                var msg = {
+                    title: LOCALE.errors.nsfw_channel.title,
+                    description: LOCALE.errors.nsfw_channel.description
+                }
+                return resolve(message.channel.send(
+                    Utils.createSimpleEmbed(msg.title, msg.description)
+                ));
             }
             const tagged_user = message.mentions.users.entries().next()
             var user = message.author
             if (tagged_user.value) user = tagged_user.value[1];
             var user_pic = user.avatarURL({
-                format: "png",
+                format: "jpg",
                 size: 512
             })
             if (!user_pic) {
