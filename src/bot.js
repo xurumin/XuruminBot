58afd62235c191b5cc1565972bb3cfe464cd57de
jonatas araujo <jonatasaraujofx@gmail.com>
2021-04-28 19:44:57 -0300

    fix vote

--- src/bot.js
+++ src/bot.js
@@ -292,7 +292,6 @@ const init = async () => {
 					const prize = Utils.random(450, 550);
 					var user = client.users.cache.find(user => user.id == lastVotes[0].id)
 					if (!user) return;
-
 					user.send(new Discord.MessageEmbed().setTitle(`:star2: Obrigado por votar no Xurumin! :star2:`).setDescription(`\nComo recompensa, você ganhou **X$${prize}**!\n(você pode usar \`x!profile\` em um servidor para ver quantos X$ você tem)\n\nContinue votando para ganhar mais!`).setColor("#9d65c9"))
 					await Payment.fastPayXurumin(user.id, prize)
 				} catch (error) {
