bca43077d0ec5aeb6c0adfa2f57cbad44d758002
jonatas araujo <jonatasaraujofx@gmail.com>
2021-06-23 22:36:33 -0300

    temp fix ytdl 7... wow

--- src/plugins/Music/utils/MusicPlayer.js
+++ src/plugins/Music/utils/MusicPlayer.js
@@ -4,7 +4,7 @@ const Music = require("./../utils/Music")
 const urlQ = require("url")
 // const ytdl = require("ytdl-core")
 const path = require("path")
-const ytdl = require(path.join(__dirname, "./../../../libs/ytdl-temp/lib/index"))
+const ytdl = require(path.join(__dirname, "./../../../libs/ytdl-temp/"))
 
 class MusicPlayer {
     /**
