75729127fedc4cf5c30d1efd29f1f9178e8d5414
jonatas araujo <jonatasaraujofx@gmail.com>
2021-06-23 22:24:37 -0300

    temp fix ytdl 4

--- src/plugins/Music/utils/MusicPlayer.js
+++ src/plugins/Music/utils/MusicPlayer.js
@@ -3,7 +3,7 @@ const Utils = require("./../../../utils/utils")
 const Music = require("./../utils/Music")
 const urlQ = require("url")
 // const ytdl = require("ytdl-core")
-const ytdl = require("./../../../libs/ytdl-temp/")
+const ytdl = require("./../../../libs/ytdl-temp/lib/index")
 
 
 class MusicPlayer {
