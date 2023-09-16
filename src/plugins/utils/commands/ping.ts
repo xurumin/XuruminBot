f5e35057166d0c0c9a7e5eaece69d1404841951a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-16 17:17:48 -0300

    feat: improve ping

--- src/plugins/utils/commands/ping.ts
+++ src/plugins/utils/commands/ping.ts
@@ -5,7 +5,9 @@ import { Response } from '../../../libs/PluginManager/@types/Response';
 export default class PingCommand implements Command {
   name: string = "ping";
   description: string = "Pong! Ou pong...?";
-  async execute(request: Request, response: Response) {    
-   await response.send(`Pong in ${Date.now() - request.createdAt}ms`);
+  async execute(request: Request, _: Response) {
+    const start = Date.now();
+    await request.interaction?.createMessage("Ping...?");
+    await request.interaction?.editOriginalMessage(`Pong in ${Date.now() - start}ms`);
   }
 }
